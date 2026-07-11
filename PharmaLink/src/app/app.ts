96ce314cb88b53b048ab191fc538dcc6b8a42708
ziadhanii <UG_31107103@ics.tanta.edu.eg>
2026-07-11 04:59:00 +0300

    feat: initialize PharmaLink Angular application structure
    - Add main application files including app component, routing, and styles.
    - Create core, shared, layouts, and pages directories for organized structure.
    - Set up TypeScript configuration for the application and testing.
    - Include basic HTML template and main entry point for bootstrapping the app.
    - Remove outdated README and package.json files.

--- PharmaLink/src/app/app.ts
+++ PharmaLink/src/app/app.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('PharmaLink-Front-End');
+  protected readonly title = signal('PharmaLink');
 }
