dcff0dce7915b03974c833eeaa5b6344086b2aac
ziadhanii <UG_31107103@ics.tanta.edu.eg>
2026-07-14 23:20:37 +0300

    feat: Add PrimeNG

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,30 @@
 import { Component, signal } from '@angular/core';
 
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
+import { TagModule } from 'primeng/tag';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { AvatarModule } from 'primeng/avatar';
+import { DividerModule } from 'primeng/divider';
+
 @Component({
   selector: 'app-root',
-  imports: [],
+  standalone: true,
+  imports: [
+    ButtonModule,
+    CardModule,
+    InputTextModule,
+    CheckboxModule,
+    TagModule,
+    ProgressBarModule,
+    AvatarModule,
+    DividerModule,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('PharmaLink');
+  protected readonly title = signal('Pharma Link');
 }
