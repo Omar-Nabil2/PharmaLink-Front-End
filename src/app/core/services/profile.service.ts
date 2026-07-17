1789e40cfed462f4bd15f444a79668d4e964cc23
ziadhanii <UG_31107103@ics.tanta.edu.eg>
2026-07-17 05:20:56 +0300

    refactor: improve code formatting and streamline profile service methods

--- src/app/core/services/profile.service.ts
+++ src/app/core/services/profile.service.ts
@@ -2,33 +2,35 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '@environments/environment';
-import { GetPharmacyProfileResponse, UpdatePatientProfileRequest, UpdatePharmacyProfileRequest, UpdatePharmacyProfileResponse, PatientProfileResponse } from '../interfaces/profile.interface';
+import {
+  GetPharmacyProfileResponse,
+  UpdatePatientProfileRequest,
+  UpdatePharmacyProfileRequest,
+  UpdatePharmacyProfileResponse,
+  PatientProfileResponse,
+} from '../interfaces/profile.interface';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProfileService {
-    private readonly baseUrl = environment.baseUrl;
+  private readonly baseUrl = environment.baseUrl;
 
+  constructor(private readonly http: HttpClient) {}
 
-    constructor(private readonly http: HttpClient) { }
+  getProfile(): Observable<GetPharmacyProfileResponse> {
+    return this.http.get<GetPharmacyProfileResponse>(`${this.baseUrl}/Pharmaciest`);
+  }
 
-    getProfile(): Observable<GetPharmacyProfileResponse> {
+  getPatientProfile(): Observable<PatientProfileResponse> {
+    return this.http.get<PatientProfileResponse>(`${this.baseUrl}patients/profile`);
+  }
 
-        return this.http.get<GetPharmacyProfileResponse>(`${this.baseUrl}/Pharmaciest`);
-    }
+  updatePatientProfile(data: UpdatePatientProfileRequest): Observable<PatientProfileResponse> {
+    return this.http.put<PatientProfileResponse>(`${this.baseUrl}patients/profile`, data);
+  }
 
-    getPatientProfile(): Observable<PatientProfileResponse> {
-        const url = this.omarUrl || this.baseUrl;
-        return this.http.get<PatientProfileResponse>(`${url}patients/profile`);
-    }
-
-    updatePatientProfile(data: UpdatePatientProfileRequest): Observable<PatientProfileResponse> {
-        const url = this.omarUrl || this.baseUrl;
-        return this.http.put<PatientProfileResponse>(`${url}patients/profile`, data);
-    }
-
-    updateProfile(data: UpdatePharmacyProfileRequest): Observable<UpdatePharmacyProfileResponse> {
-        return this.http.put<UpdatePharmacyProfileResponse>(`${this.baseUrl}/Pharmaciest`, data);
-    }
+  updateProfile(data: UpdatePharmacyProfileRequest): Observable<UpdatePharmacyProfileResponse> {
+    return this.http.put<UpdatePharmacyProfileResponse>(`${this.baseUrl}/Pharmaciest`, data);
+  }
 }
