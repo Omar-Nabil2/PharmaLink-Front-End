1051103de19076ac99fdc0c10aefea75f17f83d1
ziadhanii <UG_31107103@ics.tanta.edu.eg>
2026-07-22 13:37:31 +0300

    config: adjust base and local URLs across development and production environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  baseUrl: 'https://pharmalink.tryasp.net/api/v1',
+  baseUrl: 'https://localhost:5001/api/v1',
   localUrl: 'https://localhost:5001/api/v1',
 };
