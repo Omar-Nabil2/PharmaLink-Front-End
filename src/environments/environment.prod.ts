e0414a7c6e479868d2ee0912b3fb86130686e038
ziadhanii <UG_31107103@ics.tanta.edu.eg>
2026-08-15 03:55:09 +0300

    chore: Update Google Client ID

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   baseUrl: 'https://localhost:5001/api/v1',
   vapidPublicKey: 'BAwqFkplI2QPz8FFzrseWM3sxXt2jjGpFkWpoJAAXn_0MNOfJqNS-q8CrfTGvejeG5JgEKsZWiZ80nDRJOxP-HU',
-  googleClientId: 'YOUR_GOOGLE_CLIENT_ID'
+  googleClientId: '976498159528-olrqkr43qik6aon7302n5vrdl214957e.apps.googleusercontent.com'
 };
